43c3e75dc0ae6f99eb4ebc93658499426858a039
Tung <nguyenthanhtung1211@gmail.com>
2016-06-06 23:41:40 +0800

    add material classes

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,13 +1,14 @@
 angular.module('app.routes', [])
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    $ionicConfigProvider.tabs.position('bottom');
 
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
     // Each state's controller can be found in controllers.js
     $stateProvider
-
 
       .state('tabsController.sumExchangeRates', {
         url: '/sum',
@@ -46,6 +47,4 @@ angular.module('app.routes', [])
       })
 
     $urlRouterProvider.otherwise('/home/sum')
-
-
   });
